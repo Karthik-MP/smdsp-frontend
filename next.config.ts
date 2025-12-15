56ba96d756f332feb6cf93acb0b6f79c268d5e1b
Karthik MP <karthikmp521@gmail.com>
2025-12-15 16:14:39 -0500

    updating build fixes

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,28 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  serverExternalPackages: ["@copilotkit/runtime"],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Exclude problematic files from being processed
+      config.externals = config.externals || [];
+      config.externals.push({
+        "thread-stream": "commonjs thread-stream",
+        pino: "commonjs pino",
+        "pino-pretty": "commonjs pino-pretty",
+      });
+    }
+    
+    // Ignore test files
+    config.module = config.module || {};
+    config.module.rules = config.module.rules || [];
+    config.module.rules.push({
+      test: /node_modules[\\/]thread-stream[\\/](test|bench\.js|LICENSE|README\.md)/,
+      use: "null-loader",
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
