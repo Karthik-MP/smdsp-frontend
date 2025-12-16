792b1c3b6436d43a7904af352e6aa27eb4f26d9c
Karthik MP <karthikmp521@gmail.com>
2025-12-15 22:20:22 -0500

    updating build

--- next.config.ts
+++ next.config.ts
@@ -1,43 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  allowedDevOrigins: [
-    "local-origin.dev",
-    "*.local-origin.dev",
-    "128.226.206.249",
+  redirects: async () => [
+    {
+      source: "/",
+      destination: "/chan",
+      permanent: false,
+    },
   ],
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/chan",
-        permanent: false,
-      },
-    ];
-  },
-  serverExternalPackages: ["@copilotkit/runtime"],
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Exclude problematic files from being processed
-      config.externals = config.externals || [];
-      config.externals.push({
-        "thread-stream": "commonjs thread-stream",
-        pino: "commonjs pino",
-        "pino-pretty": "commonjs pino-pretty",
-      });
-    }
-    
-    // Ignore test files
-    config.module = config.module || {};
-    config.module.rules = config.module.rules || [];
-    config.module.rules.push({
-      test: /node_modules[\\/]thread-stream[\\/](test|bench\.js|LICENSE|README\.md)/,
-      use: "null-loader",
-    });
-
-    return config;
-  },
 };
 
 export default nextConfig;
